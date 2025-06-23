894af9aff831a40811b80e0e9d323aac000880fd
GoldPapaya <lucasdcrawford32@gmail.com>
2025-06-23 19:40:17 -0400

    made switchcase start menu

--- main.go
+++ main.go
@@ -11,14 +11,32 @@ func main() {
 	fmt.Scan(&username)
 	// validate username
 
-	var startScreenInput uint
-
 	fmt.Printf("Welcome %v. Please input a number according to the options below:\n", username)
+	startMenu()
+
+}
+
+func startMenu() {
+	var startMenuInput uint
+
 	fmt.Println("1 - Play")
 	fmt.Println("2 - Rules")
 	fmt.Println("3 - Balance")
 	fmt.Println("4 - Ledger")
-	fmt.Scan(&startScreenInput)
+	fmt.Scan(&startMenuInput)
 
-	fmt.Printf("%v", startScreenInput)
+	switch startMenuInput {
+	case 1:
+		// play
+		fmt.Println("option 1")
+	case 2:
+		// rules
+		fmt.Println("option 2")
+	case 3:
+		// balance
+		fmt.Println("option 3")
+	case 4:
+		// ledger
+		fmt.Println("option 4")
+	}
 }
