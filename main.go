14adb1ed221c6ff6be4883a6067706e478ef19ca
GoldPapaya <lucasdcrawford32@gmail.com>
2025-06-23 09:57:53 -0400

    added start screen

--- main.go
+++ main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	var username string
+
+	fmt.Println("Welcome to the Blackjack Casino! Please enter your name.")
+	fmt.Scan(&username)
+	// validate username
+
+	var startScreenInput uint
+
+	fmt.Printf("Welcome %v. Please input a number according to the options below:\n", username)
+	fmt.Println("1 - Play")
+	fmt.Println("2 - Rules")
+	fmt.Println("3 - Balance")
+	fmt.Println("4 - Ledger")
+	fmt.Scan(&startScreenInput)
+
+	fmt.Printf("%v", startScreenInput)
 }
