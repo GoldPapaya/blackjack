299dc4dd3d66893591d4c819c6220396894641ea
GoldPapaya <lucasdcrawford32@gmail.com>
2025-06-24 13:48:14 -0400

    added new card object and dynamic gethandvalue function

--- main.go
+++ main.go
@@ -4,6 +4,37 @@ import (
 	"fmt"
 )
 
+// Card object
+type Card struct {
+	face string
+	value uint
+}
+
+func getHandValue(hand []Card) uint {
+	var total uint = 0
+	numberOfAces := 0
+	
+	// Calculate total value without aces, and count the number of aces
+	for _, card := range hand {
+		if card.face == "Ace" {
+			numberOfAces += 1
+		} else {
+			total += card.value
+		}
+	}
+
+	// Calculate total value of aces and add to total
+	for i := 0; i < numberOfAces; i++ {
+		if total + 11 <= 21 {
+			total += 11
+		} else {
+			total += 1
+		}
+	}
+
+	return total
+}
+
 func main() {
 	var username string
 
@@ -14,6 +45,29 @@ func main() {
 	fmt.Printf("Welcome %v. Please input a number according to the options below:\n", username)
 	startScreen()
 
+	// game loop
+	for {
+
+		// TODO testing stuff below, not final
+		var playerCard = Card{face: "King", value: 3}
+		var houseCard = Card{face: "Ace"}
+		var houseCard2 = Card{face: "Ace"}
+
+		playerHand := []Card{
+			playerCard,
+		}
+		houseHand := []Card{
+			houseCard,
+			houseCard2,
+		}
+
+
+		fmt.Println("A new round of Blackjack has started.")
+		fmt.Printf("You have been dealt an %v, %v", playerCard, getHandValue(playerHand))
+		fmt.Printf("The house has an %v, %v", houseCard, getHandValue(houseHand))
+		fmt.Println("1 - Hit")
+		fmt.Println("2 - Hold")
+	}
 }
 
 func startScreen() {
