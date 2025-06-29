4af3e5e01e0ec263f66d19f30491890bbde1cf40
GoldPapaya <lucasdcrawford32@gmail.com>
2025-06-28 22:49:49 -0400

    remove card from deck once drawn

--- main.go
+++ main.go
@@ -93,6 +93,70 @@ var deck = []Card{
 	{face: "King", suit: "Clubs", value: 10},
 }
 
+func initDeck() []Card {
+	var deck = []Card{
+	{face: "Ace", suit: "Spades"},
+	{face: "Ace", suit: "Hearts"},
+	{face: "Ace", suit: "Diamonds"},
+	{face: "Ace", suit: "Clubs"},
+	{face: "Two", suit: "Spades", value: 2},
+	{face: "Two", suit: "Hearts", value: 2},
+	{face: "Two", suit: "Diamonds", value: 2},
+	{face: "Two", suit: "Clubs", value: 2},
+	{face: "Three", suit: "Spades", value: 3},
+	{face: "Three", suit: "Hearts", value: 3},
+	{face: "Three", suit: "Diamonds", value: 3},
+	{face: "Three", suit: "Clubs", value: 3},
+	{face: "Four", suit: "Spades", value: 4},
+	{face: "Four", suit: "Hearts", value: 4},
+	{face: "Four", suit: "Diamonds", value: 4},
+	{face: "Four", suit: "Clubs", value: 4},
+	{face: "Five", suit: "Spades", value: 5},
+	{face: "Five", suit: "Hearts", value: 5},
+	{face: "Five", suit: "Diamonds", value: 5},
+	{face: "Five", suit: "Clubs", value: 5},
+	{face: "Six", suit: "Spades", value: 6},
+	{face: "Six", suit: "Hearts", value: 6},
+	{face: "Six", suit: "Diamonds", value: 6},
+	{face: "Six", suit: "Clubs", value: 6},
+	{face: "Seven", suit: "Spades", value: 7},
+	{face: "Seven", suit: "Hearts", value: 7},
+	{face: "Seven", suit: "Diamonds", value: 7},
+	{face: "Seven", suit: "Clubs", value: 7},
+	{face: "Eight", suit: "Spades", value: 8},
+	{face: "Eight", suit: "Hearts", value: 8},
+	{face: "Eight", suit: "Diamonds", value: 8},
+	{face: "Eight", suit: "Clubs", value: 8},
+	{face: "Nine", suit: "Spades", value: 9},
+	{face: "Nine", suit: "Hearts", value: 9},
+	{face: "Nine", suit: "Diamonds", value: 9},
+	{face: "Nine", suit: "Clubs", value: 9},
+	{face: "Ten", suit: "Spades", value: 10},
+	{face: "Ten", suit: "Hearts", value: 10},
+	{face: "Ten", suit: "Diamonds", value: 10},
+	{face: "Ten", suit: "Clubs", value: 10},
+	{face: "Jack", suit: "Spades", value: 10},
+	{face: "Jack", suit: "Hearts", value: 10},
+	{face: "Jack", suit: "Diamonds", value: 10},
+	{face: "Jack", suit: "Clubs", value: 10},
+	{face: "Queen", suit: "Spades", value: 10},
+	{face: "Queen", suit: "Hearts", value: 10},
+	{face: "Queen", suit: "Diamonds", value: 10},
+	{face: "Queen", suit: "Clubs", value: 10},
+	{face: "King", suit: "Spades", value: 10},
+	{face: "King", suit: "Hearts", value: 10},
+	{face: "King", suit: "Diamonds", value: 10},
+	{face: "King", suit: "Clubs", value: 10}}
+	return deck
+}
+
+func drawCard(deck []Card) Card {
+	index := rand.Intn(len(deck))
+	card := deck[index]
+	deck = append(deck[:index], deck[index+1:]...)
+	return card
+}
+
 func main() {
 	var username string
 
@@ -105,21 +169,27 @@ func main() {
 
 	playerHand := []Card{}
 	houseHand := []Card{}
+	deck = initDeck()
 	fmt.Println("A new round of Blackjack has started.")
 	// game loop
 	for {
 		var gameInput int
 
-		var playerCard = deck[rand.Intn(len(deck))]
-		var houseCard = deck[rand.Intn(len(deck))]
-		playerHand = append(playerHand, playerCard)
-		houseHand = append(houseHand, houseCard)
+		fmt.Printf("deck1%v", deck)
+		var playerCard1 = drawCard(deck)
+		var playerCard2 = drawCard(deck)
+		var houseCard1 = drawCard(deck)
+		var houseCard2 = drawCard(deck)
+
+		playerHand = append(playerHand, playerCard1, playerCard2)
+		houseHand = append(houseHand, houseCard1, houseCard2)
 
 
-		fmt.Printf("You have been dealt an %v, %v, %v\n", playerCard, playerHand, getHandValue(playerHand))
-		fmt.Printf("The house has an %v, %v, %v\n", houseCard, houseHand, getHandValue(houseHand))
+		fmt.Printf("You have been dealt an %v, %v, %v\n", playerCard1, playerCard2, getHandValue(playerHand))
+		fmt.Printf("The house has an %v, %v, %v\n", houseCard1, houseCard2, getHandValue(houseHand))
 		fmt.Println("1 - Hit")
 		fmt.Println("2 - Hold")
+		fmt.Printf("deck2%v", deck)
 		fmt.Scan(&gameInput)
 		if gameInput == 2 {
 			break
